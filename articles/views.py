bbd3b9bb5c287d68af660ff7f640beb90bc01fad
Shonak <shonak_soni@hotmail.com>
2020-05-04 19:02:51 +0530

    first commit

---
+++ articles/views.py
@@ -0,0 +1,38 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from . import models
+
+class ArticleListView(ListView):
+    model = models.Article
+    template_name = 'article_list.html'
+    login_url = 'login'
+
+class ArticleDetailView(DetailView):
+    model = models.Article
+    template_name = 'article_detail.html'
+    login_url = 'login'
+
+class ArticleUpdateView(UpdateView):
+    model = models.Article
+    fields = ['title', 'body', ]
+    template_name = 'article_edit.html'
+    login_url = 'login'
+
+class ArticleDeleteView(DeleteView):
+    model = models.Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+    login_url = 'login'
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = models.Article
+    template_name = 'article_new.html'
+    fields = ['title', 'body',]
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
